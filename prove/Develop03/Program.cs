561e36f150267111c228d4d7a42bb5d3d8c37cf5
Esmith22306 <Ethankip@gmail.com>
2024-02-17 13:17:35 -0700

    Commit

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Scripture scripture1 = new Scripture("3 Nephi ", 5, 13, "Behold, I am a disciple of Jesus Christ, the Son of God. I have been called of him to declare his word among his people, that they might have everlasting life. ");
+        Scripture scripture1 = new Scripture("2 Nephi ", 31, 20, "Wherefore, ye must press forward with a steadfastness in Christ, having a perfect brightness of hope, and a love of God and of all men. Wherefore, if ye shall press forward, feasting upon the word of Christ, and endure to the end, behold, thus saith the Father: Ye shall have eternal life.");
         Console.Clear();
 
         scripture1.Display();
