1851507ab8724ef1280c7708bc32dbd0b5268acc
Esmith22306 <Ethankip@gmail.com>
2024-03-12 12:55:51 -0600

    Commit

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+    
+    List<Shape> shapes = new List<Shape>();
+
+
+    Square s1 = new Square("green", 3);
+    shapes.Add(s1);
+
+    Rectangle s2 = new Rectangle("red", 3, 4);
+    shapes.Add(s2);
+
+
+
+
+    foreach (Shape i in shapes)
+    {
+        string color = i.GetColor();
+
+        double area = i.GetArea();
+
+        Console.WriteLine($"Shape color: {color} \\ Shape Area: {area}.");
     }
+    }
+
 }
+
+
