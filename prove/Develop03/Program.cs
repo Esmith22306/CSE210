14d56f96d472d4ecd57d40ea0e07bad9fcf8923a
Esmith22306 <Ethankip@gmail.com>
2024-03-12 11:57:32 -0600

    Added loop in  Main. Modified the Scripture class to handle hiding words.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,34 +7,28 @@ class Program
         Scripture scripture1 = new Scripture("2 Nephi ", 31, 20, "Wherefore, ye must press forward with a steadfastness in Christ, having a perfect brightness of hope, and a love of God and of all men. Wherefore, if ye shall press forward, feasting upon the word of Christ, and endure to the end, behold, thus saith the Father: Ye shall have eternal life.");
         Console.Clear();
 
-        scripture1.Display();
-        Console.WriteLine("\nPress Enter to continue or type 'Q' to exit.");
-
-    
-        var input = Console.ReadKey();
-        if (input.Key == ConsoleKey.Spacebar || input.Key == ConsoleKey.Enter)
+        while (!scripture1.AllHidden())
         {
+            Console.WriteLine("Press Enter to continue or type 'quit' to exit.");
+            var input = Console.ReadLine();
 
+            if (input.ToLower() == "quit")
+                break;
+
+            Console.Clear();
+            scripture1.HideWords();
             scripture1.Display();
         }
-        // if (word.AllHidden = true)
-        // {
-        //     Console.WriteLine("\nYou got this, you memorized the scripture!!");
-            
-        // }
 
-        else if (input.Key == ConsoleKey.Q)
+    
+        if (scripture1.AllHidden())
         {
-            Environment.Exit(0);
+            Console.WriteLine("\nYou memorized the scripture!!");
         }
 
-        Console.Clear();
-
-
-        
-
-
-
-
+        Console.WriteLine("\nPress any key to exit.");
+        Console.ReadKey();
     }
+
+
 }
